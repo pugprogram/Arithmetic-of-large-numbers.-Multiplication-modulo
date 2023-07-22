7db50279134450c70ce2eada6741d9b790182c6b
pugprogram <108753375+pugprogram@users.noreply.github.com>
2023-07-22 18:37:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,20 +26,30 @@
 } LINK;
 
 
-unsigned long long read_num_from_file(){
+unsigned long long* read_num_from_file(unsigned long long* array){
+    array[0]=0;
+    array[1]=0;
     FILE* fp;
-    char name[]="exampe.txt";
+    char name[]="example.txt";
     unsigned long long number = 0;
     if ((fp= fopen(name,"r"))==NULL){
         printf("Error!");
-        return -1;
+        array[0]=-1;
+        array[1]=-1;
+        return array;
     }
     char one_symbol;
+    bool flag=false;
     while ((one_symbol=fgetc(fp))!=EOF){
-        if (one_symbol=='\n') break;
-        number=number*10+one_symbol-'0';
+        if ((one_symbol=='\n') and (!flag)){
+            flag=true;
+            continue;
+        }
+        if ((one_symbol=='\n') and (flag)) break;
+        if (!flag) array[0]=array[0]*10+one_symbol-'0';
+        if (flag) array[1]=array[1]*10+one_symbol-'0';
     }
-    return number;
+    return array;
 }
 
 //Проверка работоспособности производилась на переводе в двоичную систему счисления
@@ -57,18 +67,45 @@ LINK* save_in_Link(LINK* p, unsigned long long number){
     return p;
 }
 
+unsigned long long Print_Link_decimal_system(LINK *p, int num,unsigned long long answer){
+    if (p!=NULL){
+        unsigned long long dopper = p->pair.n;
+        int num2=num;
+        while (num2){
+            dopper=dopper<<63;
+            dopper=dopper<<1;
+            num2--;
+        }
+        answer=answer+dopper;
+        answer=Print_Link_decimal_system(p->foll,num+1,answer);
+    }
+    return answer;
+}
+
+
+
 void Print_Link(LINK* p){
-    
+    if (p!=NULL){
+        printf("%lld",p->pair.n);
+        Print_Link(p->foll);
+    }
 }
 
 int main(){
-    unsigned long long number_ten_system = read_num_from_file();
-    if (number_ten_system==-1) return 0;
-    LINK* p=NULL;
-    p=save_in_Link(p,number_ten_system);
-    while (p!=NULL) {
-        printf("%lld", p->pair.n);
-        p=p->foll;
-    }
+    unsigned long long* array_for_num=NULL;
+    array_for_num=(unsigned long long*)malloc(2*sizeof(unsigned long long));
+    array_for_num = read_num_from_file(array_for_num);
+    if (array_for_num[0]==-1) return 0;
+    LINK* first_num=NULL;
+    first_num=save_in_Link(first_num,array_for_num[0]);
+    LINK* second_num=NULL;
+    second_num=save_in_Link(second_num,array_for_num[1]);
+    //Print_Link(p);
+    unsigned long long answer = 0;
+    answer = Print_Link_decimal_system(first_num,0,answer);
+    printf("%lld\n",answer);
+    answer=0;
+    answer=Print_Link_decimal_system(second_num,0,answer);
+    printf("%lld",answer);
     return 0;
 }
