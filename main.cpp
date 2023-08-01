c264f13f883b2da8b6653d91c884c497c323c462
pugprogram <108753375+pugprogram@users.noreply.github.com>
2023-08-01 12:54:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ typedef struct link{
 		char c;		/* symbol: actual value. */
 /*		unsigned long u; /+* unicode symbol */ 
 		unsigned int u;  /* unicode symbol */ 
-		unsigned long long n; /*  macro-digit   */
+		unsigned long n; /*  macro-digit   */
 		unsigned short us_1, us_2;
 		} pair;
 	struct link *prec; /* ptr to preceding link */
@@ -63,6 +63,7 @@ char* read_num_from_file_2(char* chastnoe){
     int ostatok=0;
     int kolvo=0;
     char prev_symbol;
+    bool flag = false;
     while ((one_symbol=fgetc(fp))!=EOF){
         if (one_symbol=='\n'){
             res[0]=(number+'0');
@@ -77,9 +78,10 @@ char* read_num_from_file_2(char* chastnoe){
         }
         int dopper=number>>1;
         chastnoe=Copy_and_add(chastnoe,(dopper+'0'));
+        flag=true;
         number=number - (dopper<<1);
     }
-    res=Copy_and_add(res,(number+'0'));
+    if (!flag) return res;
     while (true){
         number=0;
         char* dopchastnoe;
@@ -114,32 +116,124 @@ char* read_num_from_file_2(char* chastnoe){
     return res;
 }
 
-LINK* filling_structure(LINK* p,char* binary,int i){
+LINK* filling_structure(LINK* p,char* binary,int i,bool flag_chetn,bool flag_enter_0){
     unsigned long long multiplier=1;
     p=(LINK*)malloc(sizeof(LINK));
     p->foll=NULL;
     p->prec=NULL;
     p->ptype='d';
+    if (flag_enter_0){
+        p->pair.n=0;
+        return p;
+    }
     unsigned long long num=0;
     unsigned long long dop_i=i;
-    for (i;(i<dop_i+63)&&(i<strlen(binary));i++){
+    for (i;(i<dop_i+31)&&(i<strlen(binary));i++){
         num+=(binary[i]-'0')*multiplier;
         multiplier=multiplier<<1;
-        printf("\n%lld\n",num);
     }
     p->pair.n=num;
-    if (i<(strlen(binary)-1)) p->foll=filling_structure(p->foll,binary,i);
+    if (i<(strlen(binary)-1)) p->foll=filling_structure(p->foll,binary,i,!flag_chetn,false);
+    else if (!flag_chetn) p->foll=filling_structure(p->foll,binary,i,!flag_chetn,true);
     return p;
 }
 
 
 void Print_Link(LINK* p){
     if (p!=NULL){
-        printf("%lld\n",p->pair.n);
+        printf("%ld\n",p->pair.n);
         Print_Link(p->foll);
     }
 }
 
+
+
+
+char* Add_large_numbers(char* num1, char* num2){
+    unsigned long len1=strlen(num1);
+    unsigned long len2=strlen(num2);
+    unsigned long len=0;
+    char* dopstr;
+    char* new_num1;
+    char* new_num2;
+    if (len1<len2){
+        len=len2-len1;
+        dopstr=malloc(sizeof(char)*(len2+1));
+        dopstr[len2]='\0';
+        for (unsigned long i=0;i<len;i++){
+            dopstr[i]='0';
+        }
+        for (unsigned long i=len,j=0;i<len+len1;i++,j++) {
+            dopstr[i]=num1[j];
+        }
+        new_num1=malloc(sizeof(char)*(len2+1));
+        new_num1[len2]='\0';
+        for (unsigned i=0;i<len2;i++) new_num1[i]=dopstr[i];
+        new_num2=malloc(sizeof(char)*(len2+1));
+        new_num2[len2]='\0';
+        for (unsigned i=0;i<len2;i++) new_num2[i]=num2[i];
+        free(dopstr);
+    }
+    if (len1>len2){
+        len=len1-len2;
+        dopstr=malloc(sizeof(char)*(len1+1));
+        dopstr[len1]='\0';
+        for (unsigned long i=0;i<len;i++){
+            dopstr[i]='0';
+        }
+        for (unsigned long i=len,j=0;i<len+len2;i++,j++) {
+            dopstr[i]=num2[j];
+        }
+        new_num2=malloc(sizeof(char)*(len1+1));
+        new_num2[len1]='\0';
+        for (unsigned i=0;i<len1;i++) new_num2[i]=dopstr[i];
+        new_num1=malloc(sizeof(char)*(len1+1));
+        new_num1[len1]='\0';
+        for (unsigned i=0;i<len1;i++) new_num1[i]=num1[i];
+        free(dopstr);
+    }
+    len=strlen(new_num1);
+    char* res=malloc(sizeof(char)*(len+2));
+    res[len+1]='\0';
+    for (unsigned long i=0;i<len+1;i++) res[i]='0';
+    for (unsigned long i=len,j=len-1;i>0;i--,j--){
+        int num=(new_num1[j]-'0')+(new_num2[j]-'0');
+        int dop_res=(res[i]-'0')+num;
+        res[i]=((((res[i]-'0')+num)%10)+'0');
+        res[i-1]=((((res[i-1]-'0')+dop_res)/10)+'0');
+    }
+    if (res[0]=='0'){
+        len=strlen(res);
+        dopstr=malloc(sizeof(char)*(len));
+        dopstr[len-1]='\0';
+        for (unsigned long j=1;j<len;j++) dopstr[j-1]=res[j];
+        return dopstr;
+    }
+    return res;
+}
+
+
+
+char* Return_in_decimal_system(LINK* p){
+
+
+}
+
+void Printf_in_sixteenfour_system(LINK* p){
+    if ((p!=NULL)&&(p->foll!=NULL)){
+        unsigned long long senior_coeff=p->foll->pair.n;
+        char Senior_coeff_str [30];
+        senior_coeff=senior_coeff<<32;
+        snprintf(Senior_coeff_str,sizeof Senior_coeff_str,"%lld",senior_coeff);
+        char* for_printf;
+        char junior_coeff[30];
+        snprintf(junior_coeff,sizeof junior_coeff,"%ld",p->pair.n);
+        for_printf=Add_large_numbers(Senior_coeff_str,junior_coeff);
+        for (unsigned long i=0;i<strlen(for_printf);i++) printf("%c",for_printf[i]);
+        printf("\n");
+        Printf_in_sixteenfour_system(p->foll->foll);
+    }
+}
 
 
 int main(){
@@ -149,9 +243,13 @@ int main(){
     chastnoe[0]='\0';
     char* res;
     res=read_num_from_file_2(chastnoe);
+    printf("...........Binary........\n");
     for(unsigned long long i=0;i<strlen(res);i++) printf("%c",res[i]);
-    printf("\n");
-    first_number=filling_structure(first_number,res,0);
+    printf("\n\n........2^32...........\n");
+    first_number=filling_structure(first_number,res,0,false,false);
     Print_Link(first_number);
+    printf("\n........2^64...........\n");
+    Printf_in_sixteenfour_system(first_number);
+
     return 0;
 }
